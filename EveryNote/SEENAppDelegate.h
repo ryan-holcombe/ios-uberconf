772c07ce0072a5f674af57e3a1b0629ca8f99659
Ryan Holcombe <ryan.holcombe@twcable.com>
2012-06-19 09:43:33 -0600

    Initial Commit

---
+++ EveryNote/SEENAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SEENAppDelegate.h
+//  EveryNote
+//
+//  Created by Ryan Holcombe on 6/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SEENAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
